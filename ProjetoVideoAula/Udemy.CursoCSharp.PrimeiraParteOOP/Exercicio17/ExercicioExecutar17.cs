5c88d91a525bdeda3dbf5ba10d115392f0d59eb4
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-08-24 15:20:59 -0300

    Mudanças

---
+++ ProjetoVideoAula/Udemy.CursoCSharp.PrimeiraParteOOP/Exercicio17/ExercicioExecutar17.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Udemy.CursoCSharp.PrimeiraParteOOP.Exercicio17
+{
+    class ExercicioExecutar17
+    {
+    }
+}
---
+++ ProjetoVideoAula/Udemy.CursoCSharp.PrimeiraParteOOP/Exercicio17/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Udemy.CursoCSharp.PrimeiraParteOOP.Exercicio17
+{
+    public class Funcionario : IComparable<Funcionario>
+    {
+        public string Nome { get; set; }
+
+        public double Salario { get; set; }
+
+        public Funcionario(string linhaArquivo)
+        {
+            string[] partes = linhaArquivo.Split(",");
+
+            Nome = partes[0];
+            Salario = Convert.ToDouble(partes[1], CultureInfo.InvariantCulture);
+        }
+
+
+        public override string ToString()
+        {
+            return $"Cliente: {Nome}, R$ {Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        public int CompareTo(Funcionario other)
+        {
+            return Salario.CompareTo(other.Salario);
+        }
+    }
+}
